5ffb2c46799ff8b95af8bcdb1fe7c29f514b9528
Webste <steven.weber@orbis.de>
2018-09-20 16:30:24 +0200

    Extended articles model

--- app/models/article.model.js
+++ app/models/article.model.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
 const ArticleSchema = mongoose.Schema({
-  title:  { type: String, required: true },
-  author: {type: String, required: true},
-  title_img: {type: String, required: true},
-  summary: {type: String, required: true},
-  content: {type: String, required: true},  
-  published: {type: Boolean},
-  tags: [String]
+  title:      { type: String, required: true },
+  author:     { type: String, required: true },
+  title_img:  { type: String, required: true },
+  summary:    { type: String, required: true },
+  content:    { type: String, required: true },
+  date:       { type: Date, required: true, default: Date.Now },
+  hidden:     { type: Boolean, default: true},
+  comments:   [{ author: String, body: String, published: Date }],
+  meta: {
+    votes: Number,
+    tags: String
+  }
 }, {
     timestamps: true
-});
+  });
 
 module.exports = mongoose.model('Article', ArticleSchema);
